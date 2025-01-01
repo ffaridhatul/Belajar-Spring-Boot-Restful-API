a5cab4c9b359b560d773c8576de55864ac9ec3c6
ffaridhatul <faridhatulzikri11@gmail.com>
2025-01-01 16:39:25 +0700

    Controller

--- src/main/java/enigma_camp/belajar_restful_api/controller/ErrorController.java
+++ src/main/java/enigma_camp/belajar_restful_api/controller/ErrorController.java
@@ -1,2 +1,25 @@
-package enigma_camp.belajar_restful_api.controller;public class ErrorController {
+package enigma_camp.belajar_restful_api.controller;
+
+import enigma_camp.belajar_restful_api.model.WebResponse;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestControllerAdvice
+public class ErrorController {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<WebResponse<String>> constraintViolationException(ConstraintViolationException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(WebResponse.<String>builder().errors(exception.getMessage()).build());
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<WebResponse<String>> apiException(ResponseStatusException exception) {
+        return ResponseEntity.status(exception.getStatusCode()).
+                body(WebResponse.<String>builder().errors(exception.getReason()).build());
+    }
 }
--- src/main/java/enigma_camp/belajar_restful_api/controller/UserController.java
+++ src/main/java/enigma_camp/belajar_restful_api/controller/UserController.java
@@ -1,2 +1,27 @@
-package enigma_camp.belajar_restful_api.controller;public class UserController {
+package enigma_camp.belajar_restful_api.controller;
+
+import enigma_camp.belajar_restful_api.model.RegisterUserRequest;
+import enigma_camp.belajar_restful_api.model.WebResponse;
+import enigma_camp.belajar_restful_api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+
+    @PostMapping(value = "api/users",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    
+    public WebResponse<String> register (@RequestBody RegisterUserRequest request) {
+        userService.register(request);
+        return WebResponse.<String>builder().data("OK").build();
+    }
 }
