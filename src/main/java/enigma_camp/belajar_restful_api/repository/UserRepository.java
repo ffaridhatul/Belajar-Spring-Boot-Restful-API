6a4bcd88c9850f2771289af6179249b170377dc7
ffaridhatul <faridhatulzikri11@gmail.com>
2025-01-01 16:41:24 +0700

    User Repository

--- src/main/java/enigma_camp/belajar_restful_api/repository/UserRepository.java
+++ src/main/java/enigma_camp/belajar_restful_api/repository/UserRepository.java
@@ -1,2 +1,8 @@
-package enigma_camp.belajar_restful_api.repository;public class UserRepository {
+package enigma_camp.belajar_restful_api.repository;
+
+import enigma_camp.belajar_restful_api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
 }
