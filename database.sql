684b05400dd096ad269898e959bc2680090a52d2
ffaridhatul <faridhatulzikri11@gmail.com>
2025-01-01 16:46:27 +0700

    DDL Query

--- database.sql
+++ database.sql
@@ -8,3 +8,29 @@ CREATE TABLE users
     PRIMARY KEY (username),
     UNIQUE (token)
 ) ENGINE=InnoDB;
+
+CREATE TABLE contacts
+(
+    id VARCHAR(100) NOT NULL,
+    username VARCHAR(100) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100),
+    phone VARCHAR(100),
+    email VARCHAR(100),
+    PRIMARY KEY (id),
+    FOREIGN KEY fk_users_contacts (username) REFERENCES users (username)
+)
+ENGINE=InnoDB;
+
+CREATE TABLE addresses
+(
+    id          VARCHAR(100) NOT NULL,
+    contact_id  VARCHAR(100) NOT NULL,
+    street      VARCHAR(200),
+    city        VARCHAR(100),
+    province    VARCHAR(100),
+    country     VARCHAR(100) NOT NULL,
+    postal_code VARCHAR(10),
+    PRIMARY KEY (id),
+    FOREIGN KEY fk_contacts_addresses (contact_id) REFERENCES contacts (id)
+) ENGINE InnoDB;
