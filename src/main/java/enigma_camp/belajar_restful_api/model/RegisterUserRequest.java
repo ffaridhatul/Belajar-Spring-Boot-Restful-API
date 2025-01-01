e74d58c8fe08e39d211bef5276a3bda6166bf299
ffaridhatul <faridhatulzikri11@gmail.com>
2025-01-01 16:40:01 +0700

    Users DTO

--- src/main/java/enigma_camp/belajar_restful_api/model/RegisterUserRequest.java
+++ src/main/java/enigma_camp/belajar_restful_api/model/RegisterUserRequest.java
@@ -1,2 +1,27 @@
-package enigma_camp.belajar_restful_api.model;public class RegisterUserRequest {
+package enigma_camp.belajar_restful_api.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RegisterUserRequest {
+
+    @NotBlank
+    @Size(max = 100)
+    private String username;
+
+    @NotBlank
+    @Size(max = 100)
+    private String password;
+
+    @NotBlank
+    @Size(max = 100)
+    private String name;
 }
